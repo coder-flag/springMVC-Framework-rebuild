b2f25d167df581f50cdde6af40dd4882af4c45c3
coder_flag <30384440+coder-flag@users.noreply.github.com>
2019-06-18 13:15:17 +0800

    类扫描获取类定义

--- framework/src/main/java/com/zjh/starter/MiniApplication.java
+++ framework/src/main/java/com/zjh/starter/MiniApplication.java
@@ -1,7 +1,11 @@
 package com.zjh.starter;
 
+import com.zjh.core.ClassScanner;
 import com.zjh.web.server.TomcatServer;
 import org.apache.catalina.LifecycleException;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @PackageName: com.zjh.starter
@@ -19,7 +23,13 @@ public class MiniApplication {
         TomcatServer tomcatServer = new TomcatServer(args);
         try {
             tomcatServer.startServer();
+            List<Class<?>> classList = ClassScanner.scamClasses(cls.getPackage().getName());
+            classList.forEach(it->System.out.println(it.getName()));
         } catch (LifecycleException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
